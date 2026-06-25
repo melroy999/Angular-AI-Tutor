a8b16a56c0e064f537f2a20e45122a1fee4b7b3a
melroy999 <9898970+melroy999@users.noreply.github.com>
2026-06-25 09:03:36 +0200

    [/master] Experimenting with piping operators.

--- src/app/add-recipe/add-recipe.ts
+++ src/app/add-recipe/add-recipe.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, ViewChild } from '@angular/core';
+import { Component, effect, inject, signal, ViewChild } from '@angular/core';
 import { FormBuilder, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Recipe } from '../recipe';
 import { Router, RouterLink } from '@angular/router';
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Auth } from '../auth';
+import { catchError, EMPTY, finalize, tap } from 'rxjs';
 
 @Component({
   selector: 'app-add-recipe',
@@ -21,6 +22,7 @@ export class AddRecipe {
   private readonly router = inject(Router);
   private readonly snackBar = inject(MatSnackBar);
   private readonly auth = inject(Auth);
+  protected readonly submitting = signal<boolean>(false);
 
   // Bind the form so we have a direct reference to it.
   @ViewChild('nativeFormRef') nativeForm!: NgForm;
@@ -43,6 +45,7 @@ export class AddRecipe {
   protected submit(): void {
     if (this.form.valid) {
       const rawValues = this.form.getRawValue();
+      this.submitting.set(true);
       this.recipes.addRecipe({
         id: "",
         name: rawValues.name,
@@ -51,8 +54,8 @@ export class AddRecipe {
         imgUrl: 'https://placehold.co/400x300',
         isFavorite: false,
         ingredients: [],
-      }).subscribe({
-        next: () => {
+      }).pipe(
+        tap(() => {
           this.recipes.refreshAll();
           this.form.reset();
 
@@ -60,16 +63,19 @@ export class AddRecipe {
           if (this.nativeForm) {
             this.nativeForm.resetForm();
           }
-        },
-        error: (err) => {
+        }),
+        catchError((err) => {
           console.dir(err);
           this.snackBar.open('Failed to save recipe. Please try again.', 'Dismiss', {
             duration: 5000,
             panelClass: ['error-snackbar'],
           });
-        },
-      });
-      // this.router.navigate(["/recipes"]);
+          return EMPTY;
+        }),
+        finalize(() => {
+          this.submitting.set(false);
+        })
+      ).subscribe();
     }
   }
 }
